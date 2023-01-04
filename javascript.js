0092a5702cb7788eea89127bd937a9cf9aca0eba
Royxuzeng <e0376917@u.nus.edu>
2023-01-04 20:32:55 +0800

    add simple ui for game

--- javascript.js
+++ javascript.js
@@ -38,23 +38,59 @@ function getScoreMessage(roundWinner, playerSelection, computerSelection) {
 let playerScore = 0;
 let computerScore = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("please input your choice");
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//     }
 
-    if (playerScore > computerScore) {
-        console.log("You win.");
-    } else if (playerScore < computerScore) {
-        console.log("Computer wins");
-    } else {
-        console.log("Tie.");
+//     if (playerScore > computerScore) {
+//         console.log("You win.");
+//     } else if (playerScore < computerScore) {
+//         console.log("Computer wins");
+//     } else {
+//         console.log("Tie.");
+//     }
+// }
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+rock.addEventListener('click', () => handleClick('rock'));
+paper.addEventListener('click', () => handleClick('paper'));
+scissors.addEventListener('click', () => handleClick('scissors'));
+
+function handleClick(playerSelection) {
+    if (gameEnd) {
+        return;
+    }
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+  
+    if (playerScore == 5 || computerScore == 5) {
+      endGame();
+      gameEnd = true;
     }
 }
-   
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-game();
+
+let gameEnd = false;
+function endGame() {
+    const container = document.querySelector('#container');
+    const div = document.createElement('div');
+    let res = "";
+    if (playerScore > computerScore) {
+        res = `You win.`;
+    } else if (playerScore < computerScore) {
+        res = `Computer wins`;
+    } else {
+        res = `Tie.`;
+    }
+    res += ` ${playerScore}:${computerScore}`;
+    div.textContent = res;
+
+    container.appendChild(div);
+
+}
+
 
